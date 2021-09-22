124cedf0adbe2a244e04a73f2993cfcf08a938d6
Eric <eric_stober@outlook.com>
2021-09-22 14:16:18 -0500

    adding completed 03_09

---
+++ 03_09/script.js
@@ -0,0 +1,40 @@
+/**
+ * Practice: Making methods
+ *
+ * - Create a method for each object property.
+ * - The method receives a value to match the property to be changed.
+ * - Create a simple function to replace the current property value with the received value.
+ * - Test the method by sending new values and checking the properties in the console.
+ */
+
+const backpack = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+  pocketNum: 15,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
+  changeName: function (newName) {
+    this.name = newName;
+  },
+  changeVolume: function (newVolume) {
+    this.volume = newVolume;
+  },
+  changeColor: function (newColor) {
+    this.color = newColor;
+  },
+  changePocketNum: function (newNum) {
+    this.pocketNum = newNum;
+  },
+};
+
+console.log("The backpack object:", backpack);
